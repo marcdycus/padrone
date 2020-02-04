29b6721579ad76c6f8b3a066a07e24a4d44eff65
Marc Dycus <marcdycus@yahoo.com>
2020-02-04 18:29:19 -0500

    update 2.03

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://marcdycus:password1@ds061661.mlab.com:61661/heroku_719mtbv4";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
